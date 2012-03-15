1b3c4d7b553a19742b395d6285b6d93a66d99cff
Michael Weibel <michael.weibel@gmail.com>
2012-03-15 10:25:50 +0100

    webrt candy gh-page

---
+++ public/application.js
@@ -0,0 +1,25 @@
+function showArrow(button) {
+	
+}
+
+$(document).ready(function() {
+	$('#demobutton').click(function() {
+		var showcase = $('#showcase');
+		if(showcase.hasClass('demomode')) {
+			showcase.html('');
+			showcase.removeClass('demomode');
+			$(this).text('Try the Demo').removeClass('demomode');
+			$('#demoarrow').stop(true, true).fadeOut();
+			$('body article').stop().animate({'width': 960});
+		} else {
+			$('body article').stop().animate({'width': $('body').width() - 30});
+			$('html,body').stop().animate({'scrollTop': Math.floor(showcase.offset().top) + 1});
+			showcase.html('<iframe src="http://webrtc-candy.nodester.com" scrolling="no" frameborder="0"></iframe>');
+			showcase.addClass('demomode');
+			console.log($('#showcase iframe'));
+			$('#showcase iframe').css({'width': $('body').width() - 32 + 'px', 'height': showcase.height() - 2 + 'px'});
+			$(this).text('ONLINE').addClass('demomode');
+			$('#demoarrow').stop(true, true).fadeIn();
+		}
+	});	
+});
